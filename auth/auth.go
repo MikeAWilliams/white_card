05d78ea6583b8977fe474a01d39a0bd0bd1e6a52
Mike Williams <mumakin@gmail.com>
2020-10-04 20:56:36 -0600

    first test passing

--- auth/auth.go
+++ auth/auth.go
@@ -1,5 +1,15 @@
 package auth
 
-func AddUser(email string, db Database) error {
+import "fmt"
+
+func AddUser(u User, db Database) error {
+	exists, err := db.UserExists(u.Email)
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		return fmt.Errorf("A user with email %v already exists", u.Email)
+	}
 	return nil
 }
--- auth/test/auth_test.go
+++ auth/test/auth_test.go
@@ -9,6 +9,7 @@ import (
 )
 
 type testDB struct {
+	table []auth.User
 }
 
 func (db *testDB) GetUser(email string) (auth.User, error) {
@@ -20,12 +21,26 @@ func (db *testDB) AddUser(user auth.User) error {
 }
 
 func (db *testDB) UserExists(email string) (bool, error) {
+	for _, usr := range db.table {
+		if usr.Email == email {
+			return true, nil
+		}
+	}
 	return false, nil
 }
 
+func getTestDB(users []auth.User) testDB {
+	result := testDB{}
+	result.table = users
+	return result
+}
+
 func TestAddUser(t *testing.T) {
-	db := testDB{}
-	err := auth.AddUser("e@example.com", &db)
+	db := getTestDB([]auth.User{
+		auth.User{Email: "e@example.com", Password: "password"}})
+
+	newUser := auth.User{Email: "e@example.com", Password: "whatever"}
+	err := auth.AddUser(newUser, &db)
 
 	require.NotNil(t, err)
 }
