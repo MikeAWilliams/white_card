48c2d2782645b6ae7d856f6c0417590bc015fced
Mike Williams <mumakin@gmail.com>
2020-10-11 10:24:46 -0600

    test password in signup

--- auth/auth.go
+++ auth/auth.go
@@ -4,6 +4,8 @@ import (
 	e "github.com/MikeAWilliams/white_card/wraperr"
 )
 
+type PasswordValidator func(pw string) error
+
 func AddUser(u User, db Database) *e.WrapError {
 	exists, err := db.UserExists(u.Email)
 	if err != nil {
@@ -17,6 +19,14 @@ func AddUser(u User, db Database) *e.WrapError {
 	addErr := db.AddUser(u)
 	if addErr != nil {
 		return e.Wrap(addErr, "Database error during AddUser")
+	}
+	return nil
+}
+
+func Signup(u User, db Database, pwValidator PasswordValidator) *e.WrapError {
+	err := pwValidator(u.Password)
+	if nil != err {
+		return e.Wrap(err, "There is an error with the password %v", err.Error())
 	}
 	return nil
 }
