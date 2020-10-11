33d0a7dbb7646dc286c31e4ed8fbdb3635d11af6
Mike Williams <mumakin@gmail.com>
2020-10-10 20:59:31 -0600

    add new non wrapping error contsructor

--- wraperr/test/wraperr_test.go
+++ wraperr/test/wraperr_test.go
@@ -8,8 +8,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestAddUserExists(t *testing.T) {
+func TestErrorWithMessageArgs(t *testing.T) {
 	testObject := e.Wrap(errors.New("something bad happened"), "V1=%v, V2=%v", "A bad thing", 2)
 	require.Equal(t, "V1=A bad thing, V2=2", testObject.Message)
 	require.Equal(t, "something bad happened", testObject.Inner.Error())
+}
+func TestErrorWithSimpleMessage(t *testing.T) {
+	testObject := e.Wrap(errors.New("something bad happened"), "Here is a simple message")
+	require.Equal(t, "Here is a simple message", testObject.Message)
+}
+
+func TestMakeError(t *testing.T) {
+	testObject := e.Make("something bad happened for the %vnd time", 2)
+	require.Equal(t, "something bad happened for the 2nd time", testObject.Message)
+	require.False(t, testObject.HasInnerError())
 }
--- wraperr/wraperr.go
+++ wraperr/wraperr.go
@@ -15,9 +15,21 @@ func (err WrapError) Error() string {
 	return err.Message
 }
 
+func (err WrapError) HasInnerError() bool {
+	return nil != err.Inner
+}
+
 func Wrap(err error, message string, msgArgs ...interface{}) WrapError {
 	return WrapError{
 		Inner:      err,
+		Message:    fmt.Sprintf(message, msgArgs...),
+		StackTrace: string(debug.Stack()),
+	}
+}
+
+func Make(message string, msgArgs ...interface{}) WrapError {
+	return WrapError{
+		Inner:      nil,
 		Message:    fmt.Sprintf(message, msgArgs...),
 		StackTrace: string(debug.Stack()),
 	}
