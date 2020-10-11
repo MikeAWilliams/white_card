a5d63f4302b048146a25b174af0712607d9e4056
Mike Williams <mumakin@gmail.com>
2020-10-10 20:45:48 -0600

    more tests

--- wraperr/test/wraperr_test.go
+++ wraperr/test/wraperr_test.go
@@ -9,6 +9,7 @@ import (
 )
 
 func TestAddUserExists(t *testing.T) {
-	testObject := e.Wrap(errors.New("somethign bad happened"), "V1=%v, V2=%v", "A bad thing", 2)
+	testObject := e.Wrap(errors.New("something bad happened"), "V1=%v, V2=%v", "A bad thing", 2)
 	require.Equal(t, "V1=A bad thing, V2=2", testObject.Message)
+	require.Equal(t, "something bad happened", testObject.Inner.Error())
 }
