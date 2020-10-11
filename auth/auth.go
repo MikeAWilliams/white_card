cb65750f2beb8a1fa4944c8e55f54fd43482db13
Mike Williams <mumakin@gmail.com>
2020-10-11 10:59:49 -0600

    add email link

--- auth/auth.go
+++ auth/auth.go
@@ -1,10 +1,16 @@
 package auth
 
 import (
+	"fmt"
+
 	e "github.com/MikeAWilliams/white_card/wraperr"
 )
 
 type PasswordValidator func(pw string) error
+
+type EmailSender interface {
+	Send(address string, subject string, body string) error
+}
 
 func AddUser(u User, db Database) *e.WrapError {
 	exists, err := db.UserExists(u.Email)
@@ -23,10 +29,26 @@ func AddUser(u User, db Database) *e.WrapError {
 	return nil
 }
 
-func Signup(u User, db Database, pwValidator PasswordValidator) *e.WrapError {
+func Signup(u User, db Database, pwValidator PasswordValidator, email EmailSender) *e.WrapError {
 	err := pwValidator(u.Password)
 	if nil != err {
 		return e.Wrap(err, "There is an error with the password %v", err.Error())
 	}
+
+	email.Send(u.Email, "Validate Your Eamil Address", getValidateEmailBody(u.Email))
+
 	return nil
+}
+
+func getValidateEmailBody(address string) string {
+	link := fmt.Sprintf("%v/api/v1/auth/verify?email=%v&token=%v", getBaseURL(), address, getJWT)
+	return fmt.Sprintf("Click the link below to validate your email address\r\n%v", link)
+}
+
+func getBaseURL() string {
+	return "garbage//"
+}
+
+func getJWT() string {
+	return "garbage"
 }
--- auth/test/signup_test.go
+++ auth/test/signup_test.go
@@ -8,13 +8,40 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type emailSpy struct {
+	lastAddress string
+	lastSubject string
+	lastBody    string
+}
+
+func (em *emailSpy) Send(address string, subject string, body string) error {
+	em.lastAddress = address
+	em.lastSubject = subject
+	em.lastBody = body
+	return nil
+}
+
+func TestHappyPath(t *testing.T) {
+	db := getTestDB([]auth.User{
+		auth.User{Email: "e@example.com", Password: "password"}},
+		nil, nil, nil)
+	newUser := auth.User{Email: "e@example.com", Password: "whatever"}
+	emSpy := emailSpy{}
+
+	err := auth.Signup(newUser, &db, func(_ string) error { return nil }, &emSpy)
+
+	require.Nil(t, err)
+	require.Equal(t, newUser.Email, emSpy.lastAddress)
+	require.Contains(t, emSpy.lastBody, "/api/v1/auth/verify?")
+	require.Contains(t, emSpy.lastBody, newUser.Email)
+}
 func TestPasswordInvalid(t *testing.T) {
 	db := getTestDB([]auth.User{
 		auth.User{Email: "e@example.com", Password: "password"}},
 		nil, nil, nil)
-
 	newUser := auth.User{Email: "e@example.com", Password: "whatever"}
-	err := auth.Signup(newUser, &db, func(_ string) error { return errors.New("bad bad bad") })
+
+	err := auth.Signup(newUser, &db, func(_ string) error { return errors.New("bad bad bad") }, &emailSpy{})
 
 	require.NotNil(t, err)
 	require.Contains(t, err.Message, "bad bad bad")
