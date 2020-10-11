260244ccf7dd455f0ad4c67f84405b7c25ff554c
Mike Williams <mumakin@gmail.com>
2020-10-11 10:00:20 -0600

    split out test db

--- auth/test/adduser_test.go
+++ auth/test/adduser_test.go
@@ -9,46 +9,6 @@ import (
 	"github.com/MikeAWilliams/white_card/auth"
 )
 
-type testDB struct {
-	table           []auth.User
-	userExistsError error
-	addUserError    error
-	getUserError    error
-}
-
-func (db *testDB) GetUser(email string) (auth.User, error) {
-	return auth.User{}, nil
-}
-
-func (db *testDB) AddUser(user auth.User) error {
-	if nil != db.addUserError {
-		return db.addUserError
-	}
-	db.table = append(db.table, user)
-	return nil
-}
-
-func (db *testDB) UserExists(email string) (bool, error) {
-	if nil != db.userExistsError {
-		return false, db.userExistsError
-	}
-	for _, usr := range db.table {
-		if usr.Email == email {
-			return true, nil
-		}
-	}
-	return false, nil
-}
-
-func getTestDB(users []auth.User, existErr error, addError error, getError error) testDB {
-	result := testDB{}
-	result.table = users
-	result.userExistsError = existErr
-	result.addUserError = addError
-	result.getUserError = getError
-	return result
-}
-
 func TestAddUserExists(t *testing.T) {
 	db := getTestDB([]auth.User{
 		auth.User{Email: "e@example.com", Password: "password"}},
